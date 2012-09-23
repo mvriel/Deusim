a3a29c9bc250a38384e730ff17be40892eaab428
Mike van Riel <mike.vanriel@naenius.com>
2012-09-23 22:19:16 +0200

    Setup project skeleton

---
+++ src/Naenius/Deusim/Application.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Deusim project.
+ * 
+ * @copyright Naenius 2012
+ * @link      https://github.com/mvriel/Deusim
+ */
+
+namespace Naenius\Deusim;
+
+/**
+ * Base class and entry point for the Deusim entity.
+ */
+class Application extends \Cilex\Application
+{
+
+}
